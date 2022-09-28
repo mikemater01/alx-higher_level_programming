80bd04b32c990c1e831d83139c99cba16b50d211
Michael <micfem2b@gmail>
2022-09-28 01:07:34 +0100

    task 6. List of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!bin/usr/python3
+# A function that prints a matrix of integers #
+
+def print_matrix_integer(matrix=[[]]):
+     if len(matrix) == 1 and len(matrix[0]) == 0:
+        print()
+        return
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end="")
+            if num == row[-1]:
+                print()
+            else:
+                print(" ", end="")
