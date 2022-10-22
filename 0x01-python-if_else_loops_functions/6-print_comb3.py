740ba18c5f2d60b218829f5eb5741e36c86c8e20
Michael Tubi <micfem2b@gmail.com>
2022-10-22 22:38:36 +0000

    correction|

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for m in range(i + 1, 10):
         print("{}{}".format(i, m), end="")
-        if i * 10 +j == 89:
+        if i * 10 + m == 89:
             print()
         else:
             print(", ", end="")
