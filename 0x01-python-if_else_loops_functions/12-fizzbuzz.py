6e580a6ac971975ebc4cc1ee84d0647723a50536
Michael <micfem2b@gmail>
2022-11-08 23:45:59 +0100

    task 12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,15 @@
-Files updated
+#!/usr/bin/python3
+"""Fizzbuzz."""
+
+
+def fizzbuzz():
+    """Fizzbuzz."""
+    for num in range(1, 101):
+        ans = str(num)
+        if num % 15 == 0:
+            ans = "FizzBuzz"
+        elif num % 3 == 0:
+            ans = "Fizz"
+        elif num % 5 == 0:
+            ans = "Buzz"
+        print(ans, end=" ")
