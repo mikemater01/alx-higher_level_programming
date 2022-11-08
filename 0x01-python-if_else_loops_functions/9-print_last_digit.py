03c27890762f648feded197145f8a98351dd6e41
Michael <micfem2b@gmail>
2022-11-08 22:50:34 +0100

    task 9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,10 @@
-Files updated
+#!/usr/bin/python3
+"""Print the last digit of a number."""
+
+
+def print_last_digit(number):
+    if not type(number) is int:
+        raise TypeError("Only integers are allowed")
+    digit = str(number)[-1]
+    print(digit, end="")
+    return digit
