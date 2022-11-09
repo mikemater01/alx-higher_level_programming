c0bf692e47925641a04f152e65446e272824090b
Michael <micfem2b@gmail>
2022-11-09 01:13:27 +0100

    Adv task 16. ByteCode -> Python #2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A magical function."""
+
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - 
